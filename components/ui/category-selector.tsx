8bd84dfdfd5cd0999cafcbca8a28b2a767653f06
softwareengineer012az <softwareengineer012az@gmail.com>
2025-08-08 20:51:15 +0000

    Adding the search engine in the categories

--- components/ui/category-selector.tsx
+++ components/ui/category-selector.tsx
@@ -22,38 +22,61 @@ interface CategorySelectorProps {
  */
 export const CategorySelector = ({ categories }: CategorySelectorProps) => {
   const [isBannerVisible, setIsBannerVisible] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div className="w-full font-sans">
       <div className="container mx-auto px-4 py-12">
-        <h2 className="text-3xl font-bold text-center mb-10 text-gray-800">
-          Shop by Category
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-          {categories.filter((category) =>  !category.title?.includes("Custom")).map((category) => (
-            <Link href={`/categories/${category.slug?.current}`} key={category.title}>
-              <div className="group block bg-white rounded-xl shadow-sm hover:shadow-xl transition-shadow duration-300 ease-in-out overflow-hidden transform hover:-translate-y-1">
-                <div className="relative w-full h-56">
-                  {category.image && <Image
-                    src={imageUrl(category.image)?.url()}
-                    alt={`Image for ${category.title}`}
-                    // layout="fill"
-                    // objectFit="cover"
-                    className="object-contain transition-transform duration-300 group-hover:scale-105"
+          {" "}
+          <h3 className="text-lg">Categories</h3>
+          <div className="lg:col-span-4 mb-3">
+            <input
+              type="text"
+              placeholder="Search categories..."
+              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>{" "}
+        
 
-                    fill
-                              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"                  />}
-                  
-                  {/* <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:bg-opacity-10 transition-all duration-300"></div> */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+          {" "}
+          {categories
+            .filter(
+              (category) =>
+                !category.title?.includes("Custom") &&
+                category.title?.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((category) => (
+              <Link
+                href={`/categories/${category.slug?.current}`}
+                key={category.title}
+              >
+                <div className="group block bg-white rounded-xl shadow-sm hover:shadow-xl transition-shadow duration-300 ease-in-out overflow-hidden transform hover:-translate-y-1">
+                  <div className="relative w-full h-56">
+                    {category.image && (
+                      <Image
+                        src={imageUrl(category.image)?.url()}
+                        alt={`Image for ${category.title}`}
+                        // layout="fill"
+                        // objectFit="cover"
+                        className="object-contain transition-transform duration-300 group-hover:scale-105"
+                        fill
+                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      />
+                    )}
+
+                    {/* <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:bg-opacity-10 transition-all duration-300"></div> */}
+                  </div>
+                  <div className="p-5">
+                    <h3 className="text-xl font-bold text-gray-900 capitalize text-center">
+                      {category.title}
+                    </h3>
+                  </div>
                 </div>
-                <div className="p-5">
-                  <h3 className="text-xl font-bold text-gray-900 capitalize text-center">
-                    {category.title}
-                  </h3>
-                </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ))}
         </div>
       </div>
 
@@ -61,7 +84,9 @@ export const CategorySelector = ({ categories }: CategorySelectorProps) => {
         <div className="fixed bottom-4 right-4 bg-gray-900 text-white p-4 rounded-lg shadow-lg flex items-center space-x-4 max-w-sm z-50">
           <div className="flex-grow">
             <p className="font-bold">Don&apos;t miss out!</p>
-            <p className="text-sm">Check our new arrivals and special offers.</p>
+            <p className="text-sm">
+              Check our new arrivals and special offers.
+            </p>
           </div>
           <button
             onClick={() => setIsBannerVisible(false)}
